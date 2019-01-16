18c540caac1a12eb267245e9447d7e689ffb0cc4
mosa-innotech <44779981+mosa-innotech@users.noreply.github.com>
2019-01-16 10:43:14 -0700

    Linking css files
    Linking css files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Home - Welcome to Tweeter</title>
+        <link rel="stylesheet" href="{{ URL::asset('css/tweeter.css') }} ">
+
+    </head>
+    <body>
+        <h1>Welcome to Tweeter</h1>
+        <h2>A knock off version of Twitter</h2>
+        <a href="about">About</a>
+    </body>
+</html>
