397ff201ba26ecc883a58ff879b02aab9fdcf166
mosa-innotech <44779981+mosa-innotech@users.noreply.github.com>
2019-01-16 10:04:37 -0700

    My first commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('signup', function () {
+    return view('signup');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('profile', function () {
+    return view('profile');
+});
+Route::get('posts', function () {
+    return view('posts');
+});
+Route::get('search', function () {
+    return view('search');
+});
